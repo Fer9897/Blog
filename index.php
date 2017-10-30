d083c013f4fab2611049dcca04df4703f81bbe31
Fer9897 <Fer9897@gmail.com>
2017-10-30 11:42:42 -0600

    Message

---
+++ index.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Inicio</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/starter-template.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+    <main role="main" class="container">
+
+      <div class="starter-template">
+        <h1>Fernando Emmanuel Espino Quintana</h1>
+        <div class="row">
+          <div class="col-sm">
+          <img src="/imagenes/foto.jpg" alt="foto" class="rounded-circle">
+          </div>
+          <div class="col-sm">
+          <h5>Mis habilidades</h5>
+          <ul>
+          <li class="text-info"><h6>Desarrollo de videojuegos en Unity</h6></li>
+          <li class="text-info"><h6>Desarrollo de Apps para Android</h6></li>
+          <li class="text-info"><h6>Programación en C#</h6></li>
+          <li class="text-info"><h6>Programación en Java</h6></li>
+          <li class="text-info"><h6>Programación en C++</h6></li>
+          </ul>
+          </div>
+          <div class="col-sm">
+          <h5>Mas información</h5>
+          <p><button type="button" class="btn btn-primary">Mi portafolio</button></p>
+          <p><button type="button" class="btn btn-primary">Mis proyectos</button></p>
+          <p><button type="button" class="btn btn-primary">Contacto</button></p>
+          </div>
+        </div>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-6">
+        <table class="table table-dark">
+          <thead>
+            <tr>
+              <th scope="col">Hora</th>
+              <th scope="col">Lunes</th>
+              <th scope="col">Martes</th>
+              <th scope="col">Miercoles</th>
+              <th scope="col">Jueves</th>
+              <th scope="col">Viernes</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">8:00 - 9:00</th>
+              <td>Principios de composición y diseño</td>
+              <td>Diseño de videojuegos</td>
+              <td>Diseño de videojuegos</td>
+              <td>Principios de composición y diseño</td>
+              <td>Tecnología de la programación</td>
+            </tr>
+            <tr>
+              <th scope="row">9:00 - 10:00</th>
+              <td>Principios de composición y diseño</td>
+              <td>Diseño de videojuegos</td>
+              <td>Diseño de videojuegos</td>
+              <td>Principios de composición y diseño</td>
+              <td>Tecnología de la programación</td>
+            </tr>
+            <tr>
+              <th scope="row">10:00 - 11:00</th>
+              <td>Sistemas operativos y redes</td>
+              <td>Proyecto: Desarrollo de aplicaciones interactivas</td>
+              <td>Tecnología de la programación</td>
+              <td>Sistemas operativos y redes</td>
+              <td>Proyecto: Desarrollo de aplicaciones interactivas</td>
+            </tr>
+            <tr>
+              <th scope="row">11:00 - 12:00</th>
+              <td>Sistemas operativos y redes</td>
+              <td>Proyecto: Desarrollo de aplicaciones interactivas</td>
+              <td>Tecnología de la programación</td>
+              <td>Sistemas operativos y redes</td>
+              <td>Proyecto: Desarrollo de aplicaciones interactivas</td>
+            </tr>
+        </div>
+        <div class="col-sm-6">
+        2  
+        </div>
+      </div>  
+
+    </main><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
